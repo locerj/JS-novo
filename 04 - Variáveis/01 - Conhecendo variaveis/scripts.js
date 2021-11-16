2c9dea18dd9dc65e619c61b4537e62787afd8439
Marcelo Souza <locee_rj@hotmail.com>
2021-11-16 18:56:48 -0300

    Iniciando estudo de variáveis

---
+++ 04 - Variáveis/01 - Conhecendo variaveis/scripts.js
@@ -0,0 +1,15 @@
+//var - declaração
+var clima = 'Quente'
+console.log(clima);
+clima = 'Frio'
+console.log(clima);
+
+//let - declaração
+let tempo = 'Seco';
+console.log(tempo);
+
+//const - declaração
+const temperatura = 'Calor';
+console.log(temperatura)
+temperatura = 'Morno'; //constantes não podem ser alteradas para
+console.log(temperatura)
---
+++ 04 - Variáveis/02 - Tipos dinamicos/scripts.js
@@ -0,0 +1,8 @@
+let clima;
+console.log(clima)
+
+clima = 'Frio'// string
+console.log(typeof(clima))
+
+clima = 38;// number
+console.log(typeof(clima))
