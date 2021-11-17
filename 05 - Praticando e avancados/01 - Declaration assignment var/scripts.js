390761e2ac00eb0dac12e3cf0eaeb757716dd92e
Marcelo Souza <locee_rj@hotmail.com>
2021-11-16 23:36:53 -0300

    iniciando Praticando e avançados

---
+++ 05 - Praticando e avancados/01 - Declaration assignment var/scripts.js
@@ -0,0 +1,6 @@
+var yourName;// declaration
+yourName = 'Marcelo';// assignment
+
+console.log(yourName);// conteúdo da variável
+
+console.log(typeof(yourName))// tipo da variável
---
+++ 05 - Praticando e avancados/02 - Agrupando declarações/scripts.js
@@ -0,0 +1,6 @@
+let age, isHuman // podemos fazer declaração em grupo
+// depois atribuimos os valores das variáveis
+age = 38
+isHuman = true
+
+console.log(age, isHuman)
