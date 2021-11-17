301c5a94aba834b7c5d64344d5b1866ac38d9cd6
Marcelo Souza <locee_rj@hotmail.com>
2021-11-17 11:31:53 -0300

    trocando os links de css pra 1 arquivo só

---
+++ 05 - Praticando e avancados/03 - Concatenando e interpolando variaveis/scripts.js
@@ -0,0 +1,10 @@
+console.log('Abra o arquivo scripts.js.')
+let nome, age, isHuman
+
+nome = 'Marcelo';
+age = 38;
+isHuman = true;
+
+console.log('O ' + nome + ' tem ' + age + ' anos.');// concatenando
+
+console.log(`O ${nome} tem ${age} anos.`)// interpolando
---
+++ 05 - Praticando e avancados/04 - Objects/scripts.js
@@ -0,0 +1,7 @@
+console.log("Abra o arquivo scripts.js.");
+const person = {
+  name: "John",
+  age: 21,
+  weight: 88.6,
+  isAdmin: true,
+};
